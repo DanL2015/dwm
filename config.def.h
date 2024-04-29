9d2817fb614ebfc1844f07f26eeffad5ec059276
Daniel Liu <daniel17liu@gmail.com>
2024-04-28 19:10:05 -0700

    border bar patch

--- config.def.h
+++ config.def.h
@@ -5,21 +5,27 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Iosevka Nerd Font:size=12:style=Regular" };
+static const char dmenufont[]       = "Iosevka Nerd Font:size=12";
+static const char col_gray1[] = "#020203";
+static const char col_gray2[] = "#1c252c";
+static const char col_gray3[] = "#b6beca";
+static const char col_gray4[] = "#e3e6eb";
+static const char col_red[] = "#e05f65";
+static const char col_orange[] = "#f1cf8a";
+static const char col_green[] = "#78dba9";
+static const char col_cyan[] = "#74bee9";
+static const char col_blue[] = "#70a5eb";
+static const char col_purple[] = "#c68aee";
+static const char col_borderbar[] = "#020203";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray4, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_blue, col_gray1,  col_blue  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = {"1", "2", "3", "4", "5"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -27,8 +33,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+  {"Nemo", NULL, NULL, 0, 1, -1},
+  {"CEmu | Calculator", NULL, NULL, 0, 1, -1},
 };
 
 /* layout(s) */
@@ -45,7 +51,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -58,12 +64,31 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "wezterm", NULL };
+static const char *screenshotcmd[] = {
+    "screenshot", "area", NULL};
+static const char *lockscreencmd[] = {"slock", NULL};
+
+/* Volume commands */
+static const char *upvol[] = {"pamixer", "-i", "5", NULL};
+static const char *downvol[] = {"pamixer", "-d", "5", NULL};
+static const char *mutevol[] = {"pamixer", "-t", NULL};
+
+/*Brightness commands*/
+static const char *upbri[] = {"xbacklight", "-inc", "5", NULL};
+static const char *downbri[] = {"xbacklight", "-dec", "5", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+  {MODKEY | ShiftMask, XK_s, spawn, {.v = screenshotcmd}},
+  {MODKEY | Mod1Mask, XK_x, spawn, {.v = lockscreencmd}},
+  {0, XF86XK_AudioLowerVolume, spawn, {.v = downvol}},
+  {0, XF86XK_AudioMute, spawn, {.v = mutevol}},
+  {0, XF86XK_AudioRaiseVolume, spawn, {.v = upvol}},
+  {0, XF86XK_MonBrightnessUp, spawn, {.v = upbri}},
+  {0, XF86XK_MonBrightnessDown, spawn, {.v = downbri}},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
