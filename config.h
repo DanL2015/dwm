3fdb898986f3a63fb2ecaaaecfa459279164b32c
Daniel Liu <daniel17liu@gmail.com>
2024-06-09 20:50:59 -0700

    update something again idk

--- config.h
+++ config.h
@@ -19,8 +19,8 @@ static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const unsigned int colorfultag = 1; /* 0 means use SchemeSel for selected tag */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "Iosevka Term:size=12:style=Regular", "Material\\-Design\\-Iconic\\-Font:style=Design-Iconic-Font" };
-static const char dmenufont[]       = "Iosevka Term:size=12";
+static const char *fonts[]          = { "Iosevka Term:size=14:style=Regular", "Material\\-Design\\-Iconic\\-Font:style=Design-Iconic-Font:size=16" };
+static const char dmenufont[]       = "Iosevka Term:size=14";
 static const char col_gray1[] = "#020203";
 static const char col_gray2[] = "#1c252c";
 static const char col_gray3[] = "#6b7078";
@@ -63,7 +63,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
   {"Nemo", NULL, NULL, 0, 1, -1},
-  {"CEmu | Calculator", NULL, NULL, 0, 1, -1},
+  {"CEmu", NULL, NULL, 0, 1, -1},
 };
 
 /* layout(s) */
@@ -94,6 +94,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m",  dmenumon,   "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3,   "-sb", col_gray1, "-sf", col_blue, "-p",  "Search:", NULL};
 static const char *termcmd[]  = { "wezterm", NULL };
+static const char *filecmd[]  = { "nemo", NULL };
 static const char *screenshotcmd[] = {
     "screenshot", "area", NULL};
 static const char *lockscreencmd[] = {"slock", NULL};
@@ -111,6 +112,7 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY | ShiftMask,                       XK_f, spawn,          {.v = filecmd } },
   {MODKEY | ShiftMask, XK_s, spawn, {.v = screenshotcmd}},
   {MODKEY | Mod1Mask, XK_x, spawn, {.v = lockscreencmd}},
   {0, XF86XK_AudioLowerVolume, spawn, {.v = downvol}},
@@ -151,10 +153,10 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_Left,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_Right, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_Left,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_Right, tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
